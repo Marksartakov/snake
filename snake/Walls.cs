984eea6bb93df50f454c41c4338cce92841c411d
Opilane TTHK <opilane@tthk.ee>
2022-01-20 09:46:50 +0200

    snake

---
+++ snake/Walls.cs
@@ -0,0 +1,26 @@
+﻿using SnakeOOP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    class Walls
+    {
+        private int v1;
+        private int v2;
+
+        public Walls(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        internal void Draw() => throw new NotImplementedException();
+
+        internal bool IsHit(Snake snake)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
